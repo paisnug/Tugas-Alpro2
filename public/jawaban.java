7aac3f815cfa65fd9a78b330268e541a32a8a3a3
Paisal Nugraha <paisalnugraha83@gmail.com>
2025-01-16 13:55:25 +0700

    Update jawaban.java

--- public/jawaban.java
+++ public/jawaban.java
@@ -1,3 +1,5 @@
+// tabel perkalian
+
 public class MultiplicationTable {
 
     public static void main(String[] args) {
@@ -9,3 +11,9 @@ public class MultiplicationTable {
         }
     }
 }
+
+
+// Class digunakan untuk menciptakan suatu objek dan setiap perintah java akan di mulai dengan class
+// MultiplicationTable berfungsi untuk membuat tabel perkalian
+//  int num = 5; kode tersebut merupakan kode yang akan di buat menggunakan perkalian 5 
+// for(int i = 1; i <= 10; ++i) digunakan untuk perkalian yang akan di mulai atau dikalikan dari angka 1 sampai dengan 10
